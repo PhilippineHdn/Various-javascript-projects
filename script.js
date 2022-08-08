4d295f8693eb6def56d7c1e5cd18b5c6f34ee155
Philippine <philippine.hedin@hotmail.fr>
2022-08-08 12:22:25 +0200

    parameter parenthesis

--- script.js
+++ script.js
@@ -27,6 +27,6 @@ function HandleUpdate() {
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-inputs.forEach(input => input.addEventListener('change', HandleUpdate));
+inputs.forEach((input) => input.addEventListener('change', HandleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', HandleUpdate));
 
